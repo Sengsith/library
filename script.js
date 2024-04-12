802c81e076d91ce1aaf974f8fc305f94d4b79cec
Angelo Rosario <angelorosario10@gmail.com>
2024-04-12 15:27:13 -0700

    update index.html style.css script.js
    add functionality for books / basic card styling

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function Book(name, author, pages) {
-  this.name = name;
+function Book(title, author, pages) {
+  this.title = title;
   this.author = author;
   this.pages = pages;
 }
-Book.prototype.getName = function () {
-  return this.name;
+Book.prototype.getTitle = function () {
+  return this.title;
 };
 Book.prototype.getAuthor = function () {
   return this.author;
@@ -15,13 +15,60 @@ Book.prototype.getPages = function () {
 
 const myLibrary = [];
 
-const addBookToLibrary = (name, author, pages, library) => {
-  const newBook = new Book(name, author, pages);
+const addBookToLibrary = (title, author, pages, library) => {
+  const newBook = new Book(title, author, pages);
   library.push(newBook);
 };
 
-// DEBUG:
-//addBookToLibrary("name1", "author1", 123, myLibrary);
-myLibrary.forEach((book) => {
-  console.log(`${book.getName()} by ${book.getAuthor()} with ${book.getPages()} pages.`);
+const updateBooksDisplay = (library, container) => {
+  container.textContent = "";
+
+  library.forEach((book) => {
+    const cardDiv = document.createElement("div");
+    const titleDiv = document.createElement("div");
+    const authorDiv = document.createElement("div");
+    const pagesDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    titleDiv.classList.add("card-title");
+    authorDiv.classList.add("card-author");
+    pagesDiv.classList.add("card-pages");
+
+    titleDiv.textContent = book.getTitle();
+    authorDiv.textContent = book.getAuthor();
+    pagesDiv.textContent = book.getPages();
+
+    cardDiv.appendChild(titleDiv);
+    cardDiv.appendChild(authorDiv);
+    cardDiv.appendChild(pagesDiv);
+    libraryContainer.appendChild(cardDiv);
+  });
+};
+
+const libraryContainer = document.querySelector(".library-container");
+
+const openButton = document.querySelector("[data-open-modal]");
+const closeButton = document.querySelector("[data-close-modal]");
+const modal = document.querySelector("[data-modal]");
+
+openButton.addEventListener("click", () => {
+  modal.showModal();
+});
+closeButton.addEventListener("click", () => {
+  modal.close();
+});
+
+const formBook = document.querySelector(".form-book");
+formBook.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = document.querySelector("#title");
+  const author = document.querySelector("#author");
+  const pages = document.querySelector("#pages");
+
+  addBookToLibrary(title.value, author.value, pages.value, myLibrary);
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+
+  updateBooksDisplay(myLibrary, libraryContainer);
+  modal.close();
 });
